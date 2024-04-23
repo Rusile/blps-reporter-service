edbef85f8b1957df0224b42b5c079c4f638d41e1
rusilee <ildarelmeev@yandex>
2024-04-23 21:25:14 +0300

    initial commit

---
+++ src/test/kotlin/ru/itmo/reporter/ReporterApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.itmo.reporter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReporterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
