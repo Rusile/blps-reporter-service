2e7346c8f6a3a745255e7b446f705d212e360609
rusilee <ildarelmeev@yandex>
2024-04-24 11:09:01 +0300

    remove useless component scan

--- src/main/kotlin/ru/itmo/reporter/ReporterApplication.kt
+++ src/main/kotlin/ru/itmo/reporter/ReporterApplication.kt
@@ -2,14 +2,8 @@ package ru.itmo.reporter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator
 
 @SpringBootApplication
-@ComponentScan(
-    basePackages = ["ru.itmo.reporter"],
-    nameGenerator = FullyQualifiedAnnotationBeanNameGenerator::class
-)
 class ReporterApplication
 
 fun main(args: Array<String>) {
